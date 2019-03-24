1cb7db5aec59e506d537ad1e2bcb4d251370948c
Stefan Grasu <sgrasu@utexas.edu>
2019-03-24 13:20:34 -0500

    placeholder for about page

---
+++ src/About.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+
+class About extends Component {
+
+
+  render() {
+    return (
+      <div className="About">
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container">
+          <h1 class="display-4">Fluid jumbotron</h1>
+          <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+          </div>
+        </div>
+      </div>
+      )
+  }
+}
+
+export default About
